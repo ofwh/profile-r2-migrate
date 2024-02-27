007d7e13227b8d82dd5cc5310d47ae36ae1f9c2d
luoweihua7 <luoweihua7@users.noreply.github.com>
2024-02-27 12:22:28 +0000

    commit 2024.02.27

--- assets/luoweihua7/Profiles/main/QuantumultX/Scripts/WeRead.js
+++ assets/luoweihua7/Profiles/main/QuantumultX/Scripts/WeRead.js
@@ -6,7 +6,18 @@ if (method === 'POST' && url.includes('mobileSync') && body.includes('upgrade'))
   const ret = JSON.parse(body);
   const { configsets = {} } = ret;
 
-  configsets.upgrade = 0;
+  configsets.upgrade = 0; // 是否有更新
+  configsets.allowScreenshotReport = 0;
+  configsets.metrickit_diagnostic_upload_enabled = 0;
+  configsets.metrickit_enabled = 0;
+  configsets.reader_ads_enabled = 0;
+  configsets.rmonitor_metrickit_enabled = 0;
+  configsets.rn_monitor_error = 0;
+  configsets.rn_sentry_enabled = 0;
+  configsets.rtl_cls_upload_enabled = 0;
+  configsets.rtl_upload_enabled = 0;
+  configsets.ttsTimeoutSecond = 3;
+  configsets.tts_audio_preload_count = 5;
   configsets.notice_title = 'QuantumultX Rewrited';
 
   const rewrited = JSON.stringify(ret);
