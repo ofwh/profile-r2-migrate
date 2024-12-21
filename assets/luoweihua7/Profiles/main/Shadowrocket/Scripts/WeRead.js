27c773ae4ac2c60845ecaa96ffbf095a8e60398b
luoweihua7 <luoweihua7@users.noreply.github.com>
2024-12-21 12:35:23 +0000

    commit 2024.12.21

--- assets/luoweihua7/Profiles/main/Shadowrocket/Scripts/WeRead.js
+++ assets/luoweihua7/Profiles/main/Shadowrocket/Scripts/WeRead.js
@@ -4,8 +4,11 @@ let { body = '' } = $response || {};
 
 // 升级提示弹窗
 if (method === 'POST' && url.includes('mobileSync') && body.includes('upgrade')) {
-  const ret = JSON.parse(body);
-  const { configsets = {} } = ret;
+  const bodyJSON = JSON.parse(body);
+  const { configsets = {} } = bodyJSON;
+
+  bodyJSON.gift = true;
+  bodyJSON.giftCount = 6;
 
   configsets.upgrade = 0; // 是否有更新
   configsets.allowScreenshotReport = 0;
@@ -19,9 +22,12 @@ if (method === 'POST' && url.includes('mobileSync') && body.includes('upgrade'))
   configsets.rtl_upload_enabled = 0;
   configsets.ttsTimeoutSecond = 3;
   configsets.tts_audio_preload_count = 5;
-  configsets.notice_title = 'QuantumultX Rewrited';
+  configsets.notice_title = 'Shadowrocket Rewrote';
+  configsets.showTeenModeAlert = 0;
+  configsets.detectDiskSpaceInterval = 43200;
+  configsets.normal_reward_ad = '{"timeoutSeconds":1}';
 
-  body = JSON.stringify(ret);
+  body = JSON.stringify(bodyJSON);
 }
 
 $done({ status, headers, body });
