bad6560b2f6332ecd46595321bb4102ac0815110
ofwh <ofwh@users.noreply.github.com>
2025-07-29 11:27:43 +0000

    commit 2025.07.29

--- assets/ofwh/Profiles/main/QuantumultX/Scripts/WeRead.js
+++ assets/ofwh/Profiles/main/QuantumultX/Scripts/WeRead.js
@@ -1,32 +1,36 @@
 const { method, url = '' } = $request || {};
 const { body = '' } = $response || {};
 
+const replaces = {
+  gift: true,
+  giftCount: 6,
+  upgrade: 0,
+  allowScreenshotReport: 0,
+  metrickit_diagnostic_upload_enabled: 0,
+  metrickit_enabled: 0,
+  reader_ads_enabled: 0,
+  rmonitor_metrickit_enabled: 0,
+  rn_monitor_error: 0,
+  rn_sentry_enabled: 0,
+  rtl_cls_upload_enabled: 0,
+  rtl_upload_enabled: 0,
+  ttsTimeoutSecond: 3,
+  tts_audio_preload_count: 5,
+  notice_title: 'Loon Script',
+  showTeenModeAlert: 0,
+  detectDiskSpaceInterval: 43200,
+  normal_reward_ad: '{"timeoutSeconds":1}',
+};
+
 // 升级提示弹窗
 if (method === 'POST' && url.includes('mobileSync') && body.includes('upgrade')) {
   const bodyJSON = JSON.parse(body);
-  const { configsets = {} } = bodyJSON;
+  const rewrote = JSON.stringify(bodyJSON, function (key, value) {
+    const replaceVal = replaces[key];
 
-  bodyJSON.gift = true;
-  bodyJSON.giftCount = 6;
+    return typeof replaceVal !== 'undefined' ? replaceVal : value;
+  });
 
-  configsets.upgrade = 0; // 是否有更新
-  configsets.allowScreenshotReport = 0;
-  configsets.metrickit_diagnostic_upload_enabled = 0;
-  configsets.metrickit_enabled = 0;
-  configsets.reader_ads_enabled = 0;
-  configsets.rmonitor_metrickit_enabled = 0;
-  configsets.rn_monitor_error = 0;
-  configsets.rn_sentry_enabled = 0;
-  configsets.rtl_cls_upload_enabled = 0;
-  configsets.rtl_upload_enabled = 0;
-  configsets.ttsTimeoutSecond = 3;
-  configsets.tts_audio_preload_count = 5;
-  configsets.notice_title = 'QuantumultX Rewrote';
-  configsets.showTeenModeAlert = 0;
-  configsets.detectDiskSpaceInterval = 43200;
-  configsets.normal_reward_ad = "{\"timeoutSeconds\":1}";
-
-  const rewrote = JSON.stringify(bodyJSON);
   $done(rewrote);
 } else {
   $done(body);
